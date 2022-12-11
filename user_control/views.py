0f2759871c8541d6b5ffc6ee915318d8ba28aadd
Francis Chukwudalu Okeke <okeke98@gmail.com>
2022-12-11 23:41:57 +0100

    interaction

--- user_control/views.py
+++ user_control/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.contrib.auth import authenticate
 from datetime import datetime
-from inventory_api.utils import get_access_token
+from inventory_api.utils import get_access_token, CustomPagination, get_query
 from inventory_api.custom_methods import IsAuthenticatedCustom
 
 
@@ -20,7 +20,7 @@ def add_user_activity(user, action):
     )
 
 class CreateUserView(ModelViewSet):
-    http_method_names = ['POST']
+    http_method_names = ['post']
     queryset = CustomUser.objects.all()
     serializer_class = CreateUserSerializer
     permission_classes = (IsAuthenticatedCustom, )
@@ -39,7 +39,7 @@ class CreateUserView(ModelViewSet):
         )
   
 class LoginView(ModelViewSet):
-    http_method_names = ['POST']
+    http_method_names = ['post']
     queryset = CustomUser.objects.all()
     serializer_class = LoginSerializer()
     
@@ -84,7 +84,7 @@ class LoginView(ModelViewSet):
         return Response({"access": access})
     
 class UpdatePasswordView(ModelViewSet):
-    http_method_names = ['POST']
+    http_method_names = ['post']
     queryset = CustomUser.objects.all()
     serializer_class = UpdatePasswordSerializer
     
@@ -110,7 +110,7 @@ class UpdatePasswordView(ModelViewSet):
         
 
 class MeView(ModelViewSet):
-    http_method_names = ['GET']
+    http_method_names = ['get']
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     permission_classes = (IsAuthenticatedCustom, )
@@ -121,10 +121,30 @@ class MeView(ModelViewSet):
     
 class UserActivitiesView(ModelViewSet):
     serializer_class = UserActivitiesSerializer
-    http_method_names = ["GET"]
+    http_method_names = ["get"]
     permission_classes = (IsAuthenticatedCustom, )
     queryset = UserActivities.objects.all()
     # queryset = UserActivities.objects.select_related("user")
+    pagination_class = CustomPagination
+    
+    def get_queryset(self):
+        if self.request.method.lower() != "get":
+            return self.queryset
+
+        data = self.request.query_params.dict()
+        data.pop("page", None)
+        keyword = data.pop("keyword", None)
+
+        results = self.queryset.filter(**data)
+
+        if keyword:
+            search_fields = (
+                "fullname", "email", "action"
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+        
+        return results
     
 class UsersView(ModelViewSet):
     http_method_names = ['GET']
@@ -133,7 +153,22 @@ class UsersView(ModelViewSet):
     serializer_class = CustomUserSerializer
     permission_classes = (IsAuthenticatedCustom, )
     
-    def list(self, request):
-        users = self.queryset().filter(is_superuser=False)
-        data =  self.serializer_class(users, many=True).data
-        return Response(data)  
+    def get_queryset(self):
+        if self.request.method.lower() != "get":
+            return self.queryset
+
+        data = self.request.query_params.dict()
+        data.pop("page", None)
+        keyword = data.pop("keyword", None)
+
+        results = self.queryset.filter(**data, is_superuser=False)
+
+        if keyword:
+            search_fields = (
+                "fullname", "email", "role"
+            )
+            query = get_query(keyword, search_fields)
+            results = results.filter(query)
+        
+        return results
+    
